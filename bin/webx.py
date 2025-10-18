a97e8401af5465f6eb743caee32181ed0de1ba51
itzhadao <hadao1234567891011@gmail.com>
2025-10-18 20:29:30 +0700

    Main()

--- bin/webx.py
+++ bin/webx.py
@@ -1,4 +1,22 @@
 from src.clear import clear
+from src.start import run
+import sys
+
+def main():
+  argv = sys.argv()
+  argc = len(argv)
+  the_input = argv[1]
+  
+  if argc != 2:
+    print("Usage: https://github.com/itzhadao/WebX/blob/main/README.md")
+    sys.exit()
+  else:
+    if the_input[-5:] != ".webx":
+      print("Needs to be .webx file")
+      sys.exit()
+    else:
+      run()
 
 if __name__ == "__main__":
   clear()
+  main()
